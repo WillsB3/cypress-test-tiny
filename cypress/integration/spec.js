3399048cce1b69bfbd74a3ee3f0a3f61bd8f3eee
Wills Bithrey <wills@potatolondon.com>
2019-07-22 07:21:56 +0100

    Add failing test highlighting scrollbars eating into viewport

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
 describe('page', () => {
-  it('works', () => {})
+  it('test viewport sizing', () => {
+    cy.visit('https://example.cypress.io/')
+    cy.viewport('iphone-5');
+    cy.window().then(win => {
+      expect(win.document.documentElement.scrollWidth).to.equal(320)
+    })
+  })
 })
